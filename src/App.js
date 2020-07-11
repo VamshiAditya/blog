b5182b6a571396d874db6ee017414c7c68249040
VamshiAditya <vamshiaditya@gmail.com>
2020-07-12 01:37:17 +0530

    commit Feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
+import faker from 'faker'
+import CommentDetail from './components/Commentdetail';
+import {ApprovalCard} from './components/ApprovalCard';
+
 
 function App() {
   return (
+    <React.Fragment>
+      <ApprovalCard>
+        <h4>Warning!</h4>
+        <p>Are you sure you want to do this?</p>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam" 
+          comment="Nice Blog post" 
+          time="today at 6:00PM"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+        
+      <ApprovalCard>
+        <CommentDetail
+          author="ham" 
+          comment="Nice Blog post" 
+          time="today at 5:00PM"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+     
+     <ApprovalCard>
+      <CommentDetail
+          author="Ram" 
+          comment="Nice Blog post" 
+          time="today at 4:00PM"
+          avatar={faker.image.avatar()} 
+      />
+     </ApprovalCard>
+      
+    </React.Fragment>
     
-    <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00Pm</span>
-          </div>
-          <div className="text">Nice Blog post!</div>
-        </div>
-      </div>
-    </div>
+    
   );
 }
 
